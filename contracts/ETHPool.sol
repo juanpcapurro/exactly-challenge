a8ffc439040720acf89ed56591be8ec1a99f1d99
capu <hi@capu.tech>
2021-09-06 14:29:09 -0300

    barebones minting, no burning

--- contracts/ETHPool.sol
+++ contracts/ETHPool.sol
@@ -2,7 +2,12 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import "hardhat/console.sol";
 
 contract ETHPool is ERC20{
     constructor() ERC20("ETHPOOL rewards distribution token", "POOL") { }
+
+    function mint() public payable {
+        _mint(msg.sender, msg.value);
+    }
 }
--- test/ETHPool.ts
+++ test/ETHPool.ts
@@ -1,13 +1,20 @@
 import { expect } from 'chai'
 import { ethers } from 'hardhat'
 import { ETHPool } from '../typechain'
+import { Signer, BigNumber } from 'ethers'
 
 describe('ETHPool', function () {
   let ethpool: ETHPool
-  before(async () => {
+  let signers: Array<Signer>
+  let deployer: Signer
+  const provider = ethers.provider
+  let alice: Signer
+  beforeEach(async () => {
     const ETHPoolFactory = await ethers.getContractFactory('ETHPool')
     ethpool = (await ETHPoolFactory.deploy()) as ETHPool
     await ethpool.deployed()
+    signers = await ethers.getSigners()
+    ;[deployer, alice] = signers
   })
 
   it('has name, symbol and decimals', async function () {
@@ -17,8 +24,26 @@ describe('ETHPool', function () {
   })
 
   it('rejects plain transfers', async function () {
-    const signer = (await ethers.getSigners())[0]
     // 100 wei, amount doesnt really matter
-    await expect(signer.sendTransaction({ to: ethpool.address, value: 100 })).to.be.revertedWith('no fallback')
+    await expect(alice.sendTransaction({ to: ethpool.address, value: 100 })).to.be.revertedWith('no fallback')
+    await expect(deployer.sendTransaction({ to: ethpool.address, value: 100 })).to.be.revertedWith('no fallback')
+  })
+  describe('minting', () => {
+    describe('WHEN alice mints', () => {
+      const value: BigNumber = BigNumber.from(10).mul(ethers.constants.WeiPerEther)
+      let previousAliceBalance: BigNumber
+      beforeEach(async () => {
+        previousAliceBalance = await provider.getBalance(await alice.getAddress())
+        ethpool = ethpool.connect(alice)
+        const tx = await ethpool.mint({ value })
+        await tx.wait()
+      })
+      it('THEN alices token balance is increased', async () => {
+        expect(await ethpool.balanceOf(await alice.getAddress())).to.eq(value)
+      })
+      it('AND alices eth balance is decreased', async () => {
+        expect(await provider.getBalance(await alice.getAddress())).to.be.lt(previousAliceBalance.sub(value))
+      })
+    })
   })
 })
