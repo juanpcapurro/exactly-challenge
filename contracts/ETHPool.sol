86728880b5996bf49279dec6ce179856bbb332cf
capu <hi@capu.tech>
2021-09-07 10:47:37 -0300

    tokens as shares for burning

--- contracts/ETHPool.sol
+++ contracts/ETHPool.sol
@@ -2,10 +2,10 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-// import "hardhat/console.sol";
 
 contract ETHPool is ERC20{
     address public immutable teamAddress;
+    uint256 private constant WAD=10**18;
 
     constructor() ERC20("ETHPOOL rewards distribution token", "POOL") {
         teamAddress = msg.sender;
@@ -22,8 +22,19 @@ contract ETHPool is ERC20{
     }
 
     function burn(uint256 amount) public {
+        // TODO: okay now I should get serious about reentrancy
+        uint256 ethToReturn = ethFromBurn(amount, tokenPrice());
         _burn(msg.sender, amount);
         // TODO: should I add a reentrancy guard or is doing it in the end enough?
-        payable(msg.sender).transfer(amount);
+        payable(msg.sender).transfer(ethToReturn);
+    }
+
+    // returned with precision 18
+    function tokenPrice() public view returns (uint256){
+        return address(this).balance * WAD / totalSupply();
+    }
+
+    function ethFromBurn(uint256 amount, uint256 price) public pure returns (uint256){
+        return amount*price/WAD;
     }
 }
