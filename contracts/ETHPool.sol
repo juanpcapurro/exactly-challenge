61eea543a2d26830f5450de1f1f517781b4038d6
capu <hi@capu.tech>
2021-09-07 14:38:42 -0300

    tokens as shares for minting

--- contracts/ETHPool.sol
+++ contracts/ETHPool.sol
@@ -17,19 +17,20 @@ contract ETHPool is ERC20{
 
     function mint() public payable {
         require(msg.sender != teamAddress, "team address cant mint");
-        emit Mint(msg.sender, msg.value, msg.value);
-        _mint(msg.sender, msg.value);
+        uint256 tokenAmout = tokenFromMint(msg.value, totalSupply(), address(this).balance - msg.value);
+        emit Mint(msg.sender, tokenAmout, msg.value);
+        _mint(msg.sender, tokenAmout);
     }
 
     function depositRewards() public payable {
         require(totalSupply() > 0, "mint first");
         require(msg.sender == teamAddress, "only team address can deposit");
-        emit RewardsDeposited(msg.value, tokenPrice());
+        emit RewardsDeposited(msg.value, _tokenPrice(totalSupply(), address(this).balance));
     }
 
     function burn(uint256 amount) public {
         // TODO: okay now I should get serious about reentrancy
-        uint256 ethToReturn = ethFromBurn(amount, tokenPrice());
+        uint256 ethToReturn = ethFromBurn(amount, totalSupply(), address(this).balance);
         emit Burn(msg.sender, amount, ethToReturn);
         _burn(msg.sender, amount);
         // TODO: should I add a reentrancy guard or is doing it in the end enough?
@@ -41,7 +42,15 @@ contract ETHPool is ERC20{
         return totalSupply() == 0 ? WAD : address(this).balance * WAD / totalSupply();
     }
 
-    function ethFromBurn(uint256 amount, uint256 price) public pure returns (uint256){
-        return amount*price/WAD;
+    function _tokenPrice(uint256 tokenSupply, uint256 ethBalance) private pure returns (uint256){
+        return tokenSupply == 0 ? WAD : ethBalance * WAD / tokenSupply;
+    }
+
+    function ethFromBurn(uint256 amount, uint256 tokenSupply, uint256 ethBalance) public pure returns (uint256){
+        return amount*_tokenPrice(tokenSupply, ethBalance)/WAD;
+    }
+
+    function tokenFromMint(uint256 ethAmount, uint256 tokenSupply, uint256 ethBalance) public pure returns (uint256){
+        return ethAmount*WAD/_tokenPrice(tokenSupply, ethBalance);
     }
 }
